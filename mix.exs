778db6521616be4c83de070a29736069fc74dfa2
Hidetaka Kojo <hidetaka.kojo@gmail.com>
2017-04-03 15:44:17 +0900

    fix for elxir-1.4

--- mix.exs
+++ mix.exs
@@ -3,11 +3,11 @@ defmodule Quadkey.Mixfile do
 
   def project do
     [app: :quadkey,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
